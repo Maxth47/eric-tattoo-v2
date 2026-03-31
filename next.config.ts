55519aa1f0c154a798d46dc706677e267d53c9c6
Thanh Le <thanhle.metropolia17@gmail.com>
2026-04-01 00:05:37 +0300

    clean code

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "framerusercontent.com",
       },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
     ],
   },
 };
